606ecd696c4edefe7a9d2739f81331dfd69b0dfe
Olamilesi1 <olamilesiadeola111@gmail.com>
2025-02-19 00:33:22 +0100

    edit

--- src/admin/adminComponents/AdminTable.jsx
+++ src/admin/adminComponents/AdminTable.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import style from "../adminStyles/dashboard.module.css";
 
 const Transactions = () => {
-
   return (
     <div className={style.container}>
       {/* Blur Effect when form is open */}
@@ -31,8 +30,21 @@ const Transactions = () => {
               <td>Ctadel Model School, Ajah</td>
               <td>05/01/2024</td>
               <td className={style.lea}>1</td>
-            </tr>
+               </tr>
           </tbody>
+         
+            {/* <tr>
+              <td data-label="Name">Emmanuel Favour</td>
+              <td data-label="Email Address">emmanuelfavour@gmail.com</td>
+              <td data-label="Phone Number">09067455558</td>
+              <td data-label="Gender">Female</td>
+              <td data-label="School">Citadel Model School, Ajah</td>
+              <td data-label="Date">05/01/2024</td>
+              <td className={style.lea} data-label="Leaderboard">
+                1
+              </td>
+            </tr>
+          </tbody> */}
         </table>
       </div>
     </div>
