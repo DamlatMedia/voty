32f85958b65ae95a455575b88a56d9eaf74af1b1
Olamilesi1 <olamilesiadeola111@gmail.com>
2025-08-15 15:51:02 +0100

    edit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -41,7 +41,7 @@ function About() {
               Learn Values, Answer Questions, Win Scholarships!
             </p>
             <p className={style.para}>
-              At <span className={style.bold}>Voice of the Teenagers</span> and
+              {/* At <span className={style.bold}>Voice of the Teenagers</span> and
               the Youths (VOTY), we are committed to inspiring young minds and
               shaping a brighter future. Our platform combines education,
               entertainment, and opportunities to foster moral development and
@@ -69,8 +69,38 @@ function About() {
               Together, we aim to empower the next generation. Join us as we
               inspire, educate, and reward young talents across the country.
               Together, we can create a generation of responsible, ethical, and
-              driven individuals.
-            </p>
+              driven individuals.*/}
+
+              Moral Values Development
+The process of actively absorbing and incorporating moral principles and values into one's character and behaviour which shape the mind of the youths is what VOTY.NG stands for in the course of Nation Building. It involves learning, understanding, and internalizing ethical guidelines, which then influence one's thoughts, actions, and overall conduct. It was once quoted by Lao Tzu "Watch your thoughts, they become your words, Watch your words, they become your actions. Watch your actions, they become your habits. Watch your habits, they become your character. Watch your character, it becomes or shape your destiny."
+</p> <br />
+
+<p className={style.para}>
+This process is crucial for personal development, social harmony, and building a positive society.     </p>
+ 
+<p className={style.para}> ✓ Learning and Understanding: Imbibing morals begins with learning about ethical principles, values, and standards of behaviour from various sources, including family, education, religion, and community.   </p>
+
+<p className={style.para}> ✓ Internalization: It's not enough to just know about morals; one needs to internalize them, making them a part of their own belief system and value structure.   </p>
+
+<p className={style.para}> ✓ Application: Moral imbibing involves applying these principles to real-life situations, making ethical decisions, and acting in accordance with one's moral compass.   </p>
+
+<p className={style.para}> ✓ Positive Impact: By imbibing moral values, individuals can cultivate positive character traits like honesty, integrity, empathy, and respect, which contribute to a more just and compassionate society.   </p>
+
+<p className={style.para}> ✓ Personal Growth: Moral development is an ongoing process, and imbibing moral principles throughout life helps individuals grow into more responsible, ethical, and well-rounded individuals.   </p> <br />
+ 
+<p className={style.head}>
+WHAT PROMPTED OUR ZEAL TOWARDS THIS COURSE   </p> 
+
+<p className={style.para}>
+1.1 We found paraphernalia of addiction, rebellion and deep rooted pain in the lives of many Nigerian teenagers and youths which is a pandemic. Let’s stop covering our eyes to these challenges which cut across all the classes of citizenship of the Nigerian society. The present crops of our youths are desperate for early prosperity, fame, success etc. without minding its consequences.
+ 
+Majority of these youths lack discipline, hard work, integrity, honesty but digress their strength to cybercrimes, drugs addiction, alcoholism, hookup, prostitution, kidnapping, banditry and lots of vices which have affected the sane society we used to have. It’s a burden within us and it behooves us as a citizen to start the work of redirecting their steps and attention towards positive values hence the VOTY.NG Project. When to start the work of reconciliation, redirection is now, it’s better late than never attempting at all.   </p> <br />
+ 
+<p className={style.para}> 1.2 Why voty.ng is necessary as a catalyst in this project is that it serves as a tool to bringing awareness and engaging the youths in a more positive way by imbibing moral values, ethical principles as against vices and abstaining from attitudes that are self-destructive.  </p> <br />
+ 
+<p className={style.para}> 1.3 The voty.ng app also seeks to appreciate and reward youths that imbibe the moral values and align with its principles by engaging with the mind-building contents that resides on the voty.ng app through scholarships, training, social responsibility etc.
+            </p> 
+         
           </div>
 
           <div className={style.how}>
